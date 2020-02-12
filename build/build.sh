6dace788dce1b40fb2bdecf215ff5c3c1efac4fe
Eric Njogu <kunadawa@gmail.com>
2020-02-12 11:16:38 +0300

    fix: copying packages into python's site-packages library path

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,4 @@
-mkdir -p $PREFIX/site-packages/
-cp -r api/ $PREFIX/site-packages/
+mkdir $SP_DIR/juu_object_detection_protos/
+touch $SP_DIR/juu_object_detection_protos/__init__.py
+cp -r api/ $SP_DIR/juu_object_detection_protos/
 #cp README.md $PREFIX/info
